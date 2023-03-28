c989951e0b15543964be0b63f0343662dcf571a2
Fazlay Rabbi <83054901+ifazlayrabbi@users.noreply.github.com>
2023-03-28 00:15:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require('express')
 const app = express()
 
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser') 
 app.use(bodyParser.urlencoded({extended: true}))
 
 require('dotenv').config()
